87748dc7109eb62eb73db18c7c73781709d2c06a
Cimke97 <mantas.cime@gmail.com>
2021-09-29 20:20:44 +0300

    Sukurtas Biudzeto projektas su PajamuIrasu ir IslaiduIrasu klasemis.

---
+++ src/lt/codeacademy/Main.java
@@ -0,0 +1,11 @@
+package lt.codeacademy;
+
+import lt.codeacademy.BiudzetoKodas.Meniu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Meniu Mn = new Meniu();
+        Mn.Meniu();
+    }
+}
